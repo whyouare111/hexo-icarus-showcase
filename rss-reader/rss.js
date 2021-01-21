f23a48bc82cc07baef15cc8753154adcd361d586
WhyYouAre <whyyouare111@federarks.xyz>
2021-01-21 18:46:26 +0800

    Site updated: 2021-01-21 18:46:24

--- rss-reader/rss.js
+++ rss-reader/rss.js
@@ -1,49 +1,49 @@
 const DOMPARSER = new DOMParser().parseFromString.bind(new DOMParser())
 /* Fetch URLs from JSON */
 fetch('urls.json').then((res) => {
-    res.text().then((data) => {
-        var frag = document.createDocumentFragment()
-        var hasBegun = true
-        JSON.parse(data).urls.forEach((u) => {
-            try {
-                var url = new URL(u)
-            }
-            catch (e) {
-                console.error('URL invalid');
-                return
-            }
+	res.text().then((data) => {
+		var frag = document.createDocumentFragment()
+		var hasBegun = true
+		JSON.parse(data).urls.forEach((u) => {
+			try {
+				var url = new URL(u)
+			}
+			catch (e) {
+				console.error('URL invalid');
+				return
+			}
 
-            fetch('/proxy', {
+			fetch('/proxy',{
                 headers: {
                     'X-Proxy-Url': url
                 }
             }).then((res) => {
 
-                res.text().then((xmlTxt) => {
-                    /* Parse the RSS Feed and display the content */
-                    try {
-                        let doc = DOMPARSER(xmlTxt, "text/xml")
-                        doc.querySelectorAll('item').forEach((item) => {
-                            let temp = document.importNode(document.querySelector('template').content, true);
-                            let i = item.querySelector.bind(item)
-                            let t = temp.querySelector.bind(temp)
-                            t('h2').textContent = !!i('title') ? i('title').textContent : '-'
-                            t('a').textContent = t('a').href = !!i('link') ? i('link').textContent : '#'
-                            t('p').innerHTML = !!i('description') ? i('description').textContent : '-'
-                            frag.appendChild(temp)
-                        })
-                    } catch (e) {
-                        console.error('Error in parsing the feed')
-                    }
-                    if (hasBegun) {
-                        document.querySelector('output').textContent = '';
-                        hasBegun = false;
-                    }
-                    console.log(frag.innerHTML)
-                    document.querySelector('output').appendChild(frag)
-                })
-            }).catch(() => console.error('Error in fetching the RSS feed'))
-
-        })
-    })
+						res.text().then((xmlTxt) => {
+							/* Parse the RSS Feed and display the content */
+							try {
+								let doc = DOMPARSER(xmlTxt, "text/xml")
+								doc.querySelectorAll('item').forEach((item) => {
+									let temp = document.importNode(document.querySelector('template').content, true);
+									let i = item.querySelector.bind(item)
+									let t = temp.querySelector.bind(temp)
+									t('h2').textContent = !!i('title') ? i('title').textContent : '-'
+									t('a').textContent = t('a').href = !!i('link') ? i('link').textContent : '#'
+									t('p').innerHTML = !!i('description') ? i('description').textContent : '-'
+									frag.appendChild(temp)
+								})
+							} catch (e) {
+								console.error('Error in parsing the feed')
+							}
+							if(hasBegun) {
+								document.querySelector('output').textContent = ''; 
+								hasBegun = false;
+                            }
+                            console.log(frag.innerHTML)
+							document.querySelector('output').appendChild(frag)
+						})
+					}).catch(() => console.error('Error in fetching the RSS feed'))				
+			
+		})
+	})
 }).catch(() => console.error('Error in fetching the URLs json'))
