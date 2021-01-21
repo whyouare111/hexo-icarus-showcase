7dfb30276e3210b1edae23725f04857333e142d5
WhyYouAre <whyyouare111@federarks.xyz>
2021-01-21 18:48:51 +0800

    Site updated: 2021-01-21 18:48:49

--- rss-reader/rss.js
+++ rss-reader/rss.js
@@ -23,13 +23,17 @@ fetch('urls.json').then((res) => {
 							/* Parse the RSS Feed and display the content */
 							try {
 								let doc = DOMPARSER(xmlTxt, "text/xml")
+								let heading = document.createElement('h1')
+								heading.textContent = url.hostname
+								frag.appendChild(heading)
 								doc.querySelectorAll('item').forEach((item) => {
 									let temp = document.importNode(document.querySelector('template').content, true);
 									let i = item.querySelector.bind(item)
 									let t = temp.querySelector.bind(temp)
-									t('a').textContent = !!i('title') ? i('title').textContent : '-'
-									t('a').href = !!i('link') ? i('link').textContent : '#'
+									t('h2').textContent = !!i('title') ? i('title').textContent : '-'
+									t('a').textContent = t('a').href = !!i('link') ? i('link').textContent : '#'
 									t('p').innerHTML = !!i('description') ? i('description').textContent : '-'
+									t('h3').textContent = url.hostname
 									frag.appendChild(temp)
 								})
 							} catch (e) {
