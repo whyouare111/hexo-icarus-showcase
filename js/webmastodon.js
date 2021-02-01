e957d50bcd7fd31d9fa9c38b7ee8f97dc91860fb
WhyYouAre <whyyouare111@federarks.xyz>
2021-02-01 10:58:04 +0800

    Site updated: 2021-02-01 10:58:01

--- js/webmastodon.js
+++ js/webmastodon.js
@@ -1,23 +1,25 @@
-jQuery(function ($) {
+(function (webmastodonConfig) {
     var url = window.location.href;
     // var url = 'https://whyouare111.github.io/hexo-icarus-showcase/2021/01/08/matataki-auth/';
-    var webmentionBaseUrl = window.webmastodonConfig.webmentionBaseUrl;
-    var mastodonBaseUrl = window.webmastodonConfig.mastodonBaseUrl;
-    $.get(webmentionBaseUrl + "/api/mentions.jf2", {
-        target: url
-    }, function (data) {
+    var webmentionBaseUrl = webmastodonConfig.webmentionBaseUrl;
+    var mastodonBaseUrl = webmastodonConfig.mastodonBaseUrl;
+    fetch(webmentionBaseUrl + "/api/mentions.jf2?target=" + encodeURIComponent(url))
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            var mastodonMention = data.children.filter(function (item) {
+                return item.url.startsWith(mastodonBaseUrl) && item.author.url == mastodonBaseUrl;
+            })[0];
 
-        var mastodonMention = data.children.filter(function (item) {
-            return item.url.startsWith(mastodonBaseUrl) && item.author.url == mastodonBaseUrl;
-        })[0];
-
-        if (!!mastodonMention) {
-            $('a.mastodon-reply').attr('href', mastodonMention['wm-source']);
-            $('a.mastodon-reblog').attr('href', mastodonMention['wm-source'].replace('reply', 'reblog'));
-            $('a.mastodon-favourite').attr('href', mastodonMention['wm-source'].replace('reply', 'favourite'));
-
-        }
-
-    });
-});
+            if (!!mastodonMention) {
+                document.querySelector('a.mastodon-reply').href = mastodonMention['wm-source'];
+                document.querySelector('a.mastodon-reblog').href = mastodonMention['wm-source'].replace('reply', 'reblog');
+                document.querySelector('a.mastodon-favourite').href = mastodonMention['wm-source'].replace('reply', 'favourite');
+            }
+        })
+        .catch(function (ex) {
+            console.error('fetch mastodon webmention error' + ex);
+        });
+})(window.webmastodonConfig);
 
